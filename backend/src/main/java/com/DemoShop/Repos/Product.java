38ce21ffbd97538bcaef6201a05a0239a4c8e636
Daiki Anjiki <daikianjiki@gmail.com>
2023-08-01 11:18:25 -0700

    added product repo and model

---
+++ backend/src/main/java/com/DemoShop/Repos/Product.java
@@ -0,0 +1,8 @@
+package com.DemoShop.Repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Product extends JpaRepository<Product, Long> {
+}
