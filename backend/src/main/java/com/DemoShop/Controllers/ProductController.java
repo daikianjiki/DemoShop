c6297554d86532786ff7f2f12a34d70d0e460a53
Daiki Anjiki <daikianjiki@gmail.com>
2023-08-04 09:02:41 -0700

    forgot annotation for postmapping

--- backend/src/main/java/com/DemoShop/Controllers/ProductController.java
+++ backend/src/main/java/com/DemoShop/Controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.DemoShop.Models.Product;
 import com.DemoShop.Services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @PostMapping
     public ResponseEntity<List<?>> getAllProducts() {
         return productService.getAllProducts();
     }
