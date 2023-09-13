33f2e92cace964a2556c11fa4203559c90d70d07
Daiki Anjiki <daikianjiki@gmail.com>
2023-09-13 09:57:27 -0700

    connected the backend for post request - user registration, complete

--- frontend_r/src/App.js
+++ frontend_r/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- frontend_r/src/pages/HomePage.js
+++ frontend_r/src/pages/HomePage.js
@@ -1,13 +1,20 @@
 import { useRef, useState } from "react"
+import axios from 'axios'
 
 
 export default function HomePage() {
 
     let [email, setEmail] = useState('')
     let [password, setPassword] = useState('')
+    let [name, setName] = useState('')
+    let [address, setAddress] = useState('')
+    let [phoneNumber, setPhoneNumber] = useState('')
 
     let emailRef = useRef()
     let passwordRef = useRef()
+    let nameRef = useRef()
+    let addressRef = useRef()
+    let phoneNumberRef = useRef()
 
     function emailChangeHandler(event) {
         setEmail(event.target.value)
@@ -17,35 +24,91 @@ export default function HomePage() {
         setPassword(event.target.value)
     }
 
+    function nameChangeHandler(event) {
+        setName(event.target.value)
+    }
+
+    function addressChangeHandler(event) {
+        setAddress(event.target.value)
+    }
+
+    function phoneNumberChangeHandler(event) {
+        setPhoneNumber(event.target.value)
+    }
+
+    const userData = {
+        email: email,
+        password: password,
+        name: name,
+        address: address,
+        phoneNumber: phoneNumber,
+    }
+
     function submitHanlder(event) {
         event.preventDefault()
-        console.log(emailRef.current.value)
-        console.log(passwordRef.current.value)
+
+        axios.post('http://localhost:9000/users/register', userData)
+        .then(res => {
+            console.log(res.status)
+            console.log(res.data)
+        })
 
         setEmail('')
         setPassword('')
+        setName('')
+        setAddress('')
+        setPhoneNumber('')
     }
 
     return (
         <>
             <form onSubmit={submitHanlder}>
-                <label>Email:</label>
+                <label className="form-label">Email:</label>
                 <input
+                    className="form-control"
                     ref={emailRef}
                     type="email"
                     id="email"
                     placeholder=""
-                    value={email.value}
+                    value={email}
                     onChange={emailChangeHandler} />
-                <label>Password:</label>
+                <label className="form-label">Password:</label>
                 <input 
+                    className="form-control"
                     ref={passwordRef}
                     type="password"
                     id="passowrd" 
                     placeholder=""
-                    value={password.value}
+                    value={password}
                     onChange={passwordChangeHandler} />
-                <button type="submit">Register</button>
+                <label className="form-label">Name:</label>
+                <input 
+                    className="form-control"
+                    ref={nameRef}
+                    type="text"
+                    id="name" 
+                    placeholder=""
+                    value={name}
+                    onChange={nameChangeHandler} />
+                <label className="form-label">Address:</label>
+                <input 
+                    className="form-control"
+                    ref={addressRef}
+                    type="text"
+                    id="address" 
+                    placeholder=""
+                    value={address}
+                    onChange={addressChangeHandler} />
+                <label className="form-label">Phone Number:</label>
+                <input 
+                    className="form-control"
+                    ref={phoneNumberRef}
+                    type="tel"
+                    id="phoneNumber" 
+                    placeholder=""
+                    value={phoneNumber}
+                    onChange={phoneNumberChangeHandler} />
+                <button className="btn btn-primary" type="submit">Register</button>
             </form>
         </>
     )
